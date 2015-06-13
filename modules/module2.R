54a12be3bff64561221ebe41f1345b5bf3e7a1a4
lcolladotor <lcollado@jhsph.edu>
2015-06-13 14:10:04 -0400

    Fix code style in module 2

--- modules/module2.R
+++ modules/module2.R
@@ -2,19 +2,19 @@
 print("I'm code")
 
 ## ----calcDemo, comment="", prompt=TRUE-----------------------------------
-2+2
-2*4
+2 + 2
+2 * 4
 2^3
 
 ## ----calcDemo2, comment="", prompt=TRUE----------------------------------
-2+(2*3)^2
-(1+3)/2 + 45
+2 + (2 * 3)^2
+(1 + 3) / 2 + 45
 
 ## ----assign, comment="", prompt=TRUE-------------------------------------
-x=2
+x = 2 # Same as: x <- 2
 x
-x*4
-x+2
+x * 4
+x + 2
 
 ## ---- echo = FALSE-------------------------------------------------------
 Charm_City_Circulator_Ridership <- read.csv("http://www.aejaffe.com/summerR_2015/data/Charm_City_Circulator_Ridership.csv")
@@ -53,10 +53,10 @@ length(name2)
 ## ----assign4, comment="", prompt=TRUE------------------------------------
 x + 2
 x * 3
-x + c(1,2,3,4)
+x + c(1, 2, 3, 4)
 
 ## ----assign5, comment="", prompt=TRUE------------------------------------
-y = x + c(1,2,3,4)
+y = x + c(1, 2, 3, 4)
 y 
 
 ## ----assign2, comment="", prompt=TRUE------------------------------------
