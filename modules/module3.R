3ec7fe43c7503f4a5477c2f356ca645cc96d6126
lcolladotor <lcollado@jhsph.edu>
2015-06-13 14:40:54 -0400

    Add spaces to module 3

--- modules/module3.R
+++ modules/module3.R
@@ -3,11 +3,11 @@
 ## help("str")
 
 ## ----numChar, comment="", prompt=TRUE------------------------------------
-class(c("Andrew","Jaffe"))
-class(c(1,4,7))
+class(c("Andrew", "Jaffe"))
+class(c(1, 4, 7))
 
 ## ----seq, comment="", prompt=TRUE----------------------------------------
-x = seq(from=1,to=5) # seq() is a function
+x = seq(from = 1, to = 5) # seq() is a function
 x
 class(x)
 
@@ -15,16 +15,16 @@ class(x)
 1:5
 
 ## ----factor1, comment="", prompt=TRUE------------------------------------
-x = factor(c("boy","girl","girl","boy","girl"))
+x = factor(c("boy", "girl", "girl", "boy", "girl"))
 x 
 class(x)
 
 ## ----logical1, comment="", prompt=TRUE-----------------------------------
-x = c(TRUE,FALSE,TRUE,TRUE,FALSE)
+x = c(TRUE, FALSE, TRUE, TRUE, FALSE)
 class(x)
 
 ## ----logical2, comment="", prompt=TRUE-----------------------------------
-z = c("TRUE","FALSE","TRUE","FALSE")
+z = c("TRUE", "FALSE", "TRUE", "FALSE")
 class(z)
 
 ## ----head, comment="",prompt=TRUE----------------------------------------
@@ -34,9 +34,9 @@ tail(z)
 str(z)
 
 ## ----tab, comment="",prompt=TRUE-----------------------------------------
-x = c("boy","girl","girl","boy","girl")
+x = c("boy", "girl", "girl", "boy", "girl")
 table(x)
-y = c(1,2,1,2,1)
+y = c(1, 2, 1, 2, 1)
 table(x,y)
 
 ## ----subset1, comment="",prompt=TRUE-------------------------------------
@@ -47,7 +47,7 @@ length(x1)
 ## ----subset2, comment="",prompt=TRUE-------------------------------------
 x1[1] # selecting first element
 x1[3:4] # selecting third and fourth elements
-x1[c(1,5,7)] # selecting first, fifth, and seventh elements
+x1[c(1, 5, 7)] # selecting first, fifth, and seventh elements
 
 ## ----matrix, comment="", prompt=TRUE-------------------------------------
 n = 1:9 # sequence from first number to second number incrementing by 1
@@ -56,13 +56,13 @@ mat = matrix(n, nrow = 3)
 mat
 
 ## ----subset3, comment="", prompt=TRUE------------------------------------
-mat[1,1] # individual entry: row 1, column 1
-mat[1,] # first row
-mat[,1] # first columns
+mat[1, 1] # individual entry: row 1, column 1
+mat[1, ] # first row
+mat[, 1] # first columns
 
 ## ----subset4, comment="", prompt=TRUE------------------------------------
-class(mat[1,])
-class(mat[,1])
+class(mat[1, ])
+class(mat[, 1])
 
 ## ----df1, comment="", prompt=TRUE----------------------------------------
 data(iris) ## just use some data in R already
@@ -75,20 +75,20 @@ head(iris$Petal.Length)
 class(iris$Petal.Width)
 
 ## ----subset6, comment="", prompt=TRUE------------------------------------
-head(iris[,2])
+head(iris[, 2])
 
 ## ----subset7, comment="", prompt=TRUE------------------------------------
-iris[1:3,c("Sepal.Width","Species")]
+iris[1:3, c("Sepal.Width", "Species")]
 
 ## ----df3, comment="", prompt=TRUE----------------------------------------
-x = c("Andrew","Leonardo","Shaun")
+x = c("Andrew", "Leonardo", "Shaun")
 y = 1:3
-df = data.frame(name=x, id=y)
+df = data.frame(name = x, id = y)
 df
 
 ## ----dfAdd, comment="", prompt=TRUE--------------------------------------
 iris2 = iris # copy `iris` to a new df
-iris2$Index=1:nrow(iris2)
+iris2$Index = 1:nrow(iris2)
 head(iris2)
 names(iris2)
 
