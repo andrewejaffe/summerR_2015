99dd972591ce8fa39e23f170d4d817cf05b6192e
Andrew Jaffe <andrew.jaffe@libd.org>
2015-06-13 14:28:22 -0400

    added labs

---
+++ labs/module3_lab.R
@@ -0,0 +1,30 @@
+####################
+# Module 3 - Lab
+# 6/15/15
+####################
+
+## In this lab you can use the interactive console to explore 
+## 	  but please record your commands here.  
+## Remember anything you type here can be "sent" to the console with 
+##    Cmd-Enter (OS-X) or Cntr-Enter (Windows/Linux).
+
+# 1. Load in the `CO2` dataset (which is included in R like the `iris` dataset
+
+# 2. What class is `CO2`?
+
+# 3. How many observations (rows) and variables (columns) are in the `CO2` dataset?
+
+# 4. How many different "plants" are in the data? (hint: `length` and `unique`)
+
+# 5. How many different "types" are in the data?
+
+# 6. Tabulate "type" and "treatment" - what are the dimensions of the resulting table?
+#		hint: you can assign tables to variables
+
+# 7. Create a new `data.frame` named `CO2.odd` that contains the odd rows of `CO2`
+#		hint: subsetting and `seq` (note the `by` argument)
+
+# 8. How many observations are in `CO2.odd`?
+
+# 9. What are the sums of the a) concentrations and b) uptake values in the `CO2.odd` dataset?
+#		And what are their means?
