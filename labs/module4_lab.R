084f1a1fc36b3d6f23234f76f0a219f248c014a5
Andrew Jaffe <andrew.jaffe@libd.org>
2015-06-14 12:06:02 -0400

    added module 4 and lab

---
+++ labs/module4_lab.R
@@ -0,0 +1,21 @@
+##############
+## Module 4: Data Input:
+## Exploring 'Monument' Dataset
+## 6/15/2015
+
+# 1. Change the name of the "Location.1" column to "location"
+
+# 2. How many monuments are in Baltimore (at least this collection...)?
+
+# 3. What are the (a) zip codes, (b) neighborhoods, (c) council districts,
+#	and (d) police districts that contain monuments, and how many monuments are in each?
+
+# 4. How many zip codes are in the (a) "Downtown" and (b) "Johns Hopkins Homewood" neighborhoods?
+
+# 5. How many monuments (a) do and (b) do not have an exact location/address?
+
+# 6. Which (a) zip code, (b) neighborhood, (c) council district,
+#	and (d) police district contains the most number of monuments?
+
+# 7. Try reading in the tab-delimited `Monuments-tab.txt` file from:
+# 		"http://biostat.jhsph.edu/~ajaffe/winterR_2015/data/Monuments-tab.txt"
