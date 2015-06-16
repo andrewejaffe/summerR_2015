d3111e2c0be47b2eca67a9743417b65e66726e2a
andrewejaffe <andrewejaffe@gmail.com>
2015-06-16 08:09:29 -0400

    added hw2

---
+++ hw/Homework2.R
@@ -0,0 +1,46 @@
+#############
+#### 140.886: Intro to R
+# Homework 2
+# 6/16/2015
+
+# Name: Andrew Jaffe
+
+# Instructions: 
+# 1. Download the "Charm City Circulator Ridership" dataset from OpenBaltimore: 
+#     	https://data.baltimorecity.gov/Transportation/Charm-City-Circulator-Ridership/wwvu-583r
+# 2. Consult the Charm City Circulator website (http://www.charmcitycirculator.com/) 
+#		to better understand the data that you are working with
+# 3. All questions require a code and answer, unless the question is designated with:
+#	[NC] – no code – just require a response and do not require code 
+#	[CO] – code only, which do not require a response, and only require code
+
+# Questions:
+
+# 1. How many lines are currently served by the Charm City Circulator? 
+#		What is/are the name(s) of the line(s)? [NC]
+
+# 2. How would you get to Camden Yards (the baseball stadium where the Orioles play) after class? [NC]
+
+# 3. How much does it cost for a one-way trip on the Charm City Circulator? [NC]
+
+# 4. Create a second dataset that only contains the columns: 
+#   day, date, average ridership per line, and daily. 
+#		Display the first 4 rows of this new dataset. [CO]
+
+# 5. How many lines were in service on: 
+# 	a. 01/13/2010:
+
+#	b. 06/25/2010: 
+
+#	c. 04/07/2012: 
+
+# 	d. 07/06/2012: 
+
+# 	e. 10/31/2012:
+
+# 6. How many days in the dataset did the circulator not operate (i.e. how many days had 0 riders)?
+
+# 7. What day did service start for each of the lines?
+
+# 8. Which day of the week had the highest average ridership, and what was the overall average ridership?
+
